e3e68e79ee97f64dbfffdd08d0948241532a416d
Paquitosoft <paquitosoftware@gmail.com>
2019-08-15 20:06:31 +0200

    Indentation fix

--- src/infrastructure/database/mongoose.js
+++ src/infrastructure/database/mongoose.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 module.exports.connectToMongo = function connectToMongo(mongoUrl) {
-    return new Promise((resolve, reject) => {
-        const { connection } = mongoose;
+	return new Promise((resolve, reject) => {
+		const { connection } = mongoose;
 
-        connection.on('error', reject);
-        connection.once('open', () => {
-            resolve(mongoose);
-        });
+		connection.on('error', reject);
+		connection.once('open', () => {
+			resolve(mongoose);
+		});
 
-        mongoose.connect(mongoUrl, { useNewUrlParser: true });
-    });
+		mongoose.connect(mongoUrl, { useNewUrlParser: true });
+	});
 };
